7f2fe0577dfa15029af31bfa06debc898ff9a595
rdlee097 <rdlee@hawaii.edu>
2021-02-16 10:39:00 -1000

    Create ex1.js

---
+++ Lab6/ex1.js
@@ -0,0 +1,11 @@
+age =  23;
+
+fav_num = 5;
+
+day_of_birth = 7;
+
+month_of_birth = 11;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
