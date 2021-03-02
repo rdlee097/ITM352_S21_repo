be8b5a0077a254926550551b93dd913da9cb9ef3
rdlee097 <rdlee@hawaii.edu>
2021-03-02 01:59:28 -1000

    Create ex1.js

---
+++ Lab8/ex1.js
@@ -0,0 +1,7 @@
+age_count = 1; // start the age counter
+age = 23;
+while(age_count < age) {
+    console.log(`age ${age_count}`);
+    age_count++;
+}
+console.log(`Dan is ${age_count} years old`);
