d06c6effbf2478e00163bd175781cde39f4b989d
rdlee097 <rdlee@hawaii.edu>
2021-03-02 01:59:36 -1000

    Create ex3.js

---
+++ Lab8/ex3.js
@@ -0,0 +1,7 @@
+// start the age counter
+age = 23;
+var age_count;
+for(age_count = 50; age_count > age; age_count--) {
+    console.log(`age ${age_count}`);
+}
+console.log(`Dan is ${age_count} years old`);
