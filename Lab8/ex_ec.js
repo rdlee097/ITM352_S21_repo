d844d465df97a24f972bf7066ec38b8977db2d6a
rdlee097 <rdlee@hawaii.edu>
2021-03-02 01:59:34 -1000

    Create ex_ec.js

---
+++ Lab8/ex_ec.js
@@ -0,0 +1,7 @@
+age_count = 1; // start the age counter
+age = 23;
+while(age_count < Math.sqrt(age) ) {
+    console.log(`age ${age_count}`);
+    age_count++;
+}
+console.log(`The square root of Dan's age is ${age_count} years old`);
