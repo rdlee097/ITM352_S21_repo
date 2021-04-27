c08eb39607068c8b031e63b963108cca254ff10f
rdlee097 <rdlee@hawaii.edu>
2021-04-27 01:46:38 -1000

    Update info_server_Ex2c.js

--- Lab13/info_server_Ex2c.js
+++ Lab13/info_server_Ex2c.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path);
-});
-app.get('/test', function (request, response, next) {
-    response.send("Got a GET to /test path");
+    console.log(request.method + ' to path ' + request.path 
+    + ' with query' + JSON.stringify(request.query));
     next();
+});
+
+app.get('/test', function (request, response, next) {
+    response.send('I got a request for /test');
 });
 
 app.use(express.static('./public'));
 
-app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
+app.listen(8081, function () {
+    console.log(`listening on port 8081`)
+    }
+); // note the use of an anonymous function here
